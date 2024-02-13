d55cc21e0d31f1f453fbc288c55e629175eefdea
Aimen <hdmen55@hotmail.com>
2024-02-12 21:31:48 -0500

    Update API base URL

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,7 +5,7 @@ import { z } from 'zod';
 
 // Create an Axios instance with a custom config
 export const api: AxiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL as string || '/api/v1' ,
+    baseURL: import.meta.env.VITE_API_URL as string ,
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json',
